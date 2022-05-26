304d1086f94df17893974d328a5165206686589f
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2022-05-26 12:34:37 +1000

    Exception Handled for the DTO validation.

--- src/main/java/com/example/payslipgenerator/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/payslipgenerator/exception/CustomExceptionHandler.java
@@ -5,13 +5,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
