fc5be694b812a1ffd58250e8a4c54ca80e0587a1
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2022-05-29 20:50:51 +1000

    modified calculation logic

--- src/main/java/com/example/payslipgenerator/service/serviceImpl/PaySlipGeneratorImpl.java
+++ src/main/java/com/example/payslipgenerator/service/serviceImpl/PaySlipGeneratorImpl.java
@@ -37,18 +37,6 @@ public class PaySlipGeneratorImpl implements PaySlipGenerator {
 
     private Double calculateTax(Integer annualSalary) {
         double tax = 0.0;
-//        if (annualSalary <= 18200) {
-//            tax = 0;
-//        } else if (annualSalary <= 37000) {
-//            tax = (annualSalary - 18200) * (int)0.19;
-//        } else if (annualSalary <= 87000) {
-//            tax = (annualSalary - 37000) * (int)0.325 + 3572;
-//        } else if (annualSalary <= 180000) {
-//            tax = (annualSalary - 87000) * (int)0.37 + 19822;
-//        } else {
-//            tax = (annualSalary - 180000) * (int)0.45 + 54232;
-//        }
-//        return (int) (annualSalary * tax);
 
         if (annualSalary > 18200)
             if(annualSalary <= 37000)
