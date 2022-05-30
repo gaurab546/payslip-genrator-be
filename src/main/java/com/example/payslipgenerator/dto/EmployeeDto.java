18d2e12823b66ec147ef476c7c0e28495b5aacd9
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2022-05-30 15:51:47 +1000

    combining commit

--- src/main/java/com/example/payslipgenerator/dto/EmployeeDto.java
+++ src/main/java/com/example/payslipgenerator/dto/EmployeeDto.java
@@ -1,6 +1,8 @@
 package com.example.payslipgenerator.dto;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
@@ -14,6 +16,10 @@ public class EmployeeDto {
     private Integer annualSalary;
     @Positive(message = "Please pass a non negative double value for Super Rate")
     private Double superRate;
+
+    @Min(value = 0, message = "Must be between 0 (Jan) and 11 (Dec) including")
+    @Max(value = 11, message = "Must be between 0 (Jan) and 11 (Dec) including")
+    private Integer paymentMonth;
 
     public String getFirstName() {
         return firstName;
@@ -47,14 +53,23 @@ public class EmployeeDto {
         this.superRate = superRate;
     }
 
-    public EmployeeDto(String firstName, String lastName, Integer annualSalary, Double superRate) {
+    public EmployeeDto(String firstName, String lastName, Integer annualSalary, Double superRate, Integer paymentMonth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.annualSalary = annualSalary;
         this.superRate = superRate;
+        this.paymentMonth = paymentMonth;
     }
 
     public EmployeeDto() {
+    }
+
+    public Integer getPaymentMonth() {
+        return paymentMonth;
+    }
+
+    public void setPaymentMonth(Integer paymentMonth) {
+        this.paymentMonth = paymentMonth;
     }
 
     @Override
